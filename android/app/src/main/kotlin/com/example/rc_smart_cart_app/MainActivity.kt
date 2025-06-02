9ddba94a22761f518f00a1c6226e4f0420297c47
cai <qbao1607@gmail.com>
2025-06-02 17:19:17 +0700

    convert master branch to note branch

--- android/app/src/main/kotlin/com/example/rc_smart_cart_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.rc_smart_cart_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/core/constants/api_constants.dart
+++
@@ -1,5 +0,0 @@
-class ApiConstants {
-  static const String base_url = "100.118.59.7:5000/api";
-  static const String send_otp_endpoint = "$base_url/auth/send-otp";
-  static const String verifyOtpEndpoint = "$base_url/auth/verify-otp";
-}
